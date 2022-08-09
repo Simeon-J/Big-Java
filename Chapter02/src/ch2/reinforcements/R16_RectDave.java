aef2b186359cdfbdd8bea5c98c9993c261170a32
Simeon Johnson <43412706+Simeon-J@users.noreply.github.com>
2022-08-08 23:59:15 -0500

    Add files via upload

---
+++ Chapter02/src/ch2/reinforcements/R16_RectDave.java
@@ -0,0 +1,15 @@
+package ch2.reinforcements;
+
+import java.awt.Rectangle;
+
+public class R16_RectDave {
+    public static void main(String[] args) {
+        // R16 A
+        Rectangle box = new Rectangle(100,100, 50,50);
+        // R16 B
+        String dave = new String("Hello, Dave");
+
+        System.out.println(box);
+        System.out.println(dave);
+    }
+}
---
+++ Chapter02/src/ch2/reinforcements/R18_Square.java
@@ -0,0 +1,12 @@
+package ch2.reinforcements;
+
+import java.awt.Rectangle;
+
+public class R18_Square {
+    public static void main(String[] args) {
+        Rectangle square = new Rectangle(10,20,40,40);
+        System.out.println(square);
+        square = new Rectangle(20,20,40,40);
+        System.out.println(square);
+    }
+}
